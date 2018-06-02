bd5ea412b70eba4e97794cec32749a12276c6042
Dominic Eggerman <Dominic@Dominics-MBP.wireless.yale.internal>
2018-06-01 20:57:31 -0400

    Adding dropdown menus on CV

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,9 @@
+/* -------------------
+Scripts for my website
+------------------- */
+
+// Nav
+
 function openNav() {
 	document.getElementById("side-nav").style.width = "20%";
 	document.getElementById("body").style.marginLeft = "20%";
@@ -10,4 +16,23 @@ function closeNav() {
 	document.getElementById("body").style.marginLeft = "0";
 	// Opacity
 	document.body.style.backgroundColor = "#818181";
+}
+
+// Dropdown tabs
+
+function dropTab() {
+	document.getElementById("tab-drop").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.drop-btn')) {
+		var dropdowns = document.getElementsByClassName("drop-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
 }
