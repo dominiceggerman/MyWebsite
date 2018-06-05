0a57afc61ac8a2a350e339b741cdc244f4120bdc
Dominic Eggerman <dominiceggerman@gmail.com>
2018-06-05 01:03:52 -0400

    Finshed up javascript for CV tabs.

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -5,13 +5,17 @@ Scripts for my website
 // Nav
 
 function openNav() {
+	// Set side-nav width to 20% of page
 	document.getElementById("side-nav").style.width = "20%";
+	// Shift body by 20% of page
 	document.getElementById("body").style.marginLeft = "20%";
 	// Opacity
 	document.body.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
 }
 function closeNav() {
+	// Close side-nav
 	document.getElementById("side-nav").style.width = "0";
+	// Reposition body to full page
 	document.getElementById("body").style.marginLeft = "0";
 	// Opacity
 	document.body.style.backgroundColor = "#818181";
@@ -19,19 +23,18 @@ function closeNav() {
 
 
 // CV tabs
-function openTab() {
+function openTab(tabnum) {
+	// Get all of the pannel tabs
 	var pantab = document.getElementsByClassName("panel-tab");
-	var j;
-	for (j = 0; j < pantab.length; j++) {
-		pantab[j].addEventListener("click", function() {
-			this.classList.toggle("active");
-			var panel = this.nextElementSibling;
-			if (panel.style.maxHeight){
-				panel.style.maxHeight = null;
-			} else {
-				panel.style.maxHeight = panel.scrollHeight + "px";
-			}
-		});
+	// For the specific panel, toggle active class
+	pantab[tabnum].classList.toggle("active")
+	// Select the panel content - next element after panel-tab
+	var panel = pantab[tabnum].nextElementSibling;
+	// Open and close panel to reveal content
+	if (panel.style.maxHeight) {
+		panel.style.maxHeight = null;
+	} else {
+		panel.style.maxHeight = panel.scrollHeight + "px";
 	}
 }
 
